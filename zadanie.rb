9ac6346e7c03481f9fc0eca8923e44117a55c72a
Sanya <jobmasalex84@ukr.net>
2013-06-03 17:08:34 +0300

    finish zapolneniya maketa

---
+++ zadanie.rb
@@ -0,0 +1,4 @@
+person1 = {first: "first name1", last: "last name1"}
+person2 = {first: "first name2", last: "last name2"}
+person3 = {first: "first name3", last: "last name3"}
+params[:father] = person1
